d59622876900138878f8f24099186a14ba508983
Yahaya Azeez <azconcept2016@gmail.com>
2024-12-22 11:18:12 +0100

    feature(reset-password): add user reset password endpoint

---
+++ routes/resetPasswordRoute.js
@@ -0,0 +1,62 @@
+const { forgotPassword, resetPassword } = require("../controllers/resetPassword");
+
+const router = require("express").Router();
+
+/**
+ * @swagger
+ * /forgot-password:
+ *   post:
+ *     tags:
+ *      - User
+ *     summary: Update user password
+ *     description: Update user password
+ *     security:
+ *       - Authorization: []
+ *     requestBody:
+ *      required: true
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          email:
+ *            type: string
+ *     responses:
+ *      200:
+ *        description:
+ *      400:
+ *        description:
+ */
+router.route("/user/forgot-password").post(forgotPassword);
+
+/**
+ * @swagger
+ * /reset-password:
+ *   post:
+ *     tags:
+ *      - User
+ *     summary: Reset password
+ *     description: Reset password
+ *     requestBody:
+ *      required: false
+ *      content:
+ *       application/json:
+ *        schema:
+ *         type: object
+ *         properties:
+ *          token:
+ *            type: string
+ *          newPassword:
+ *            type: string
+ *          confirmNewPassword:
+ *            type: string
+ *     responses:
+ *      200:
+ *        description:
+ *      400:
+ *        description:
+ *
+ */
+router.route("/user/reset-password").post(resetPassword);
+
+module.exports = router;
